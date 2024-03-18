ca80c28045e4c92404a1589a3486c828023ee07d
TTPDE <bumjin0314@gmail.com>
2024-03-18 20:01:33 +0900

    init: BoardEntity 세팅

--- src/main/java/com/nbc/trello/Board/Board.java
+++ src/main/java/com/nbc/trello/Board/Board.java
@@ -1,5 +1,35 @@
 package com.nbc.trello.Board;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+@NoArgsConstructor
 public class Board {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long board_Id;
+
+  @Column(nullable = false)
+  private String name;
+
+  @Column(nullable = false)
+  private String color;
+
+  @Column(nullable = false)
+  private String description;
 
 }

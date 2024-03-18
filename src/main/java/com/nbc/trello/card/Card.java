e4949ccae737d3e87858cb1daac9c583c0e2a999
parkjungsub <eses565@naver.com>
2024-03-18 20:11:45 +0900

    첫 업로드

--- src/main/java/com/nbc/trello/card/Card.java
+++ src/main/java/com/nbc/trello/card/Card.java
@@ -1,8 +1,27 @@
 package com.nbc.trello.card;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
 
 @Entity
 public class Card {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    @Column
+    String name;
+    @Column
+    String description;
+    @Column
+    String Color;
+    @Column
+    LocalDateTime Deadline;
+    @Column
+    LocalDateTime CreatedAt;
+    @Column
+    LocalDateTime ModifiedAt;
 
 }

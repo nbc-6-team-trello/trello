9a6f8c4db6c750501dd11522c4a2cc60b38189c2
potatobboi <kykdydxo@naver.com>
2024-03-19 20:52:20 +0900

    feat: Todo 작성 구현
    - User, Board 안받음

--- src/main/java/com/nbc/trello/domain/todo/TodoController.java
+++ src/main/java/com/nbc/trello/domain/todo/TodoController.java
@@ -1,8 +1,30 @@
 package com.nbc.trello.domain.todo;
 
-import org.springframework.stereotype.Controller;
+import com.nbc.trello.global.response.CommonResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequiredArgsConstructor
 public class TodoController {
 
+    private final TodoService todoService;
+
+    @PostMapping("/todos")
+    ResponseEntity<CommonResponse<TodoResponseDto>> createTodo(
+        @RequestBody TodoRequestDto requestDto) {
+        TodoResponseDto responseDto = todoService.createTodo(requestDto);
+
+        return ResponseEntity.status(HttpStatus.CREATED.value()).body(
+            CommonResponse.<TodoResponseDto>builder()
+                .msg("컬럼 생성 완료")
+                .statusCode(HttpStatus.CREATED.value())
+                .data(responseDto)
+                .build()
+        );
+    }
 }
--- src/main/java/com/nbc/trello/domain/todo/TodoService.java
+++ src/main/java/com/nbc/trello/domain/todo/TodoService.java
@@ -1,8 +1,19 @@
 package com.nbc.trello.domain.todo;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class TodoService {
 
+    private final TodoRepository todoRepository;
+
+    public TodoResponseDto createTodo(TodoRequestDto requestDto) {
+        Todo todo = Todo.builder()
+            .title(requestDto.getTitle())
+            .build();
+
+        return new TodoResponseDto(todoRepository.save(todo));
+    }
 }

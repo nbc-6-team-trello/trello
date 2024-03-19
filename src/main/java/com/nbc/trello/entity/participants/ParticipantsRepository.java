a4fcb527d4be7f3d00dc7bfb28160dce5c6568e7
TTPDE <bumjin0314@gmail.com>
2024-03-19 14:13:02 +0900

    feat: Participants Repository생성

---
+++ src/main/java/com/nbc/trello/entity/participants/ParticipantsRepository.java
@@ -0,0 +1,7 @@
+package com.nbc.trello.entity.participants;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ParticipantsRepository extends JpaRepository<Participants, Long> {
+
+}

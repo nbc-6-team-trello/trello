564e84339fc46c95e8c7c960efeeb1abf7dab02e
potatobboi <kykdydxo@naver.com>
2024-03-19 20:54:14 +0900

    feat: Todo 조회 구현
    - User, Board 안받음

--- src/main/java/com/nbc/trello/domain/todo/TodoController.java
+++ src/main/java/com/nbc/trello/domain/todo/TodoController.java
@@ -1,9 +1,11 @@
 package com.nbc.trello.domain.todo;
 
 import com.nbc.trello.global.response.CommonResponse;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +26,19 @@ public class TodoController {
                 .msg("컬럼 생성 완료")
                 .statusCode(HttpStatus.CREATED.value())
                 .data(responseDto)
+                .build()
+        );
+    }
+
+    @GetMapping("/todos")
+    ResponseEntity<CommonResponse<List<TodoResponseDto>>> getTodos() {
+        List<TodoResponseDto> responseDtoList = todoService.getTodos();
+
+        return ResponseEntity.status(HttpStatus.OK.value()).body(
+            CommonResponse.<List<TodoResponseDto>>builder()
+                .msg("컬럼 조회 완료")
+                .statusCode(HttpStatus.OK.value())
+                .data(responseDtoList)
                 .build()
         );
     }
--- src/main/java/com/nbc/trello/domain/todo/TodoService.java
+++ src/main/java/com/nbc/trello/domain/todo/TodoService.java
@@ -1,6 +1,9 @@
 package com.nbc.trello.domain.todo;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,5 +18,13 @@ public class TodoService {
             .build();
 
         return new TodoResponseDto(todoRepository.save(todo));
+    }
+
+    public List<TodoResponseDto> getTodos() {
+        List<Todo> todoList = todoRepository.findAll(Sort.by(Direction.DESC, "createdAt"));
+
+        return todoList.stream()
+            .map(TodoResponseDto::new)
+            .toList();
     }
 }

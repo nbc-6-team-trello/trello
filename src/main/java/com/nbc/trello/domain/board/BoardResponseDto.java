cc03d132d00a3e76cf47ed2db149dc2e34d77cf7
TTPDE <bumjin0314@gmail.com>
2024-03-19 19:21:39 +0900

    feat : Board 전체 조회 기능 추가

--- src/main/java/com/nbc/trello/domain/board/BoardResponseDto.java
+++ src/main/java/com/nbc/trello/domain/board/BoardResponseDto.java
@@ -1,12 +1,27 @@
 package com.nbc.trello.domain.board;
 
+import jakarta.persistence.Column;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class BoardResponseDto {
 
   private Long board_id;
 
+  private String name;
+
+  private String color;
+
+  private String description;
+
+  public BoardResponseDto(Board board) {
+    this.board_id = board.getId();
+    this.name = board.getName();
+    this.color = board.getColor();
+    this.description = board.getDescription();
+  }
 }
--- src/main/java/com/nbc/trello/domain/board/BoardService.java
+++ src/main/java/com/nbc/trello/domain/board/BoardService.java
@@ -3,6 +3,7 @@ package com.nbc.trello.domain.board;
 import com.nbc.trello.domain.participants.Participants;
 import com.nbc.trello.domain.participants.ParticipantsRepository;
 import com.nbc.trello.domain.user.User;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -23,5 +24,13 @@ public class BoardService {
     participantsRepository.save(participants);
 
     return participants.toDto();
+  }
+  //보드 전체 조회
+  public List<BoardResponseDto> getBoardList() {
+    List<Board> boardList = boardRepository.findAll();
+    if(boardList.isEmpty()) {
+      throw new IllegalArgumentException("조회할 수 있는 보드가 없습니다.");
+    }
+    return boardList.stream().map(BoardResponseDto::new).toList();
   }
 }

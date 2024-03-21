00e6b0c6c7742bda3bc90ff31a99b1d829cdf206
parkjungsub <eses565@naver.com>
2024-03-21 19:09:06 +0900

    dev 브렌치 합치기

--- src/main/java/com/nbc/trello/domain/card/CardController.java
+++ src/main/java/com/nbc/trello/domain/card/CardController.java
@@ -1,9 +1,12 @@
 package com.nbc.trello.domain.card;
 
+import com.nbc.trello.domain.board.BoardResponseDto;
 import com.nbc.trello.global.response.CommonResponse;
+import com.nbc.trello.global.util.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,24 +14,24 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/boards/{boardId}/todos/{todoId}/cards")
 public class CardController {
 
     private final CardService cardService;
 
-    @PostMapping
+    @PostMapping("/boards/{board_id}/columns/{column_id}/cards")
     public ResponseEntity<CommonResponse<CardResponseDto>> createCard(
-        @PathVariable Long boardId,
-        @PathVariable Long todoId, @RequestBody CardRequestDto cardRequestDto) {
+        @PathVariable("board_id") Long boardId,
+        @PathVariable("column_id") Long todoId,
+        @RequestBody CardRequestDto cardRequestDto,
+        @AuthenticationPrincipal UserDetailsImpl userDetails) {
 
         CardResponseDto cardResponseDto = cardService.CardCreateService(boardId, todoId,
-            cardRequestDto);
+            cardRequestDto, userDetails.getUser());
 
         return ResponseEntity.ok(CommonResponse.<CardResponseDto>builder()
             .msg("카드 생성에 성공하였습니다.")
@@ -37,14 +40,15 @@ public class CardController {
             .build());
     }
 
-    @GetMapping("/{cardId}")
+    @GetMapping("/boards/{board_id}/columns/{column_id}/cards/{card_id}")
     public ResponseEntity<CommonResponse<CardCommentResponseDto>> getCard(
-        @PathVariable Long boardId,
-        @PathVariable Long todoId,
-        @PathVariable Long cardId) {
+        @PathVariable("board_id") Long boardId,
+        @PathVariable("column_id") Long todoId,
+        @PathVariable("card_id") Long card_id,
+        @AuthenticationPrincipal UserDetailsImpl userDetails) {
 
         CardCommentResponseDto cardCommentResponseDto = cardService.CardGetService(boardId,
-            todoId, cardId);
+            todoId, card_id, userDetails);
 
         return ResponseEntity.ok(CommonResponse.<CardCommentResponseDto>builder()
             .msg("카드 조회에 성공하였습니다.")
@@ -53,33 +57,55 @@ public class CardController {
             .build());
     }
 
-    @DeleteMapping("/{cardId}")
-    public ResponseEntity<CommonResponse<Void>> deleteCard(
-        @PathVariable Long boardId,
-        @PathVariable Long todoId,
-        @PathVariable Long cardId) {
+    @DeleteMapping("/boards/{board_id}/columns/{column_id}/cards/{card_id}")
+    public ResponseEntity<CommonResponse<CardResponseDto>> deleteCard(
+        @PathVariable("board_id") Long boardId,
+        @PathVariable("column_id") Long todoId,
+        @PathVariable("card_id") Long card_id,
+        @AuthenticationPrincipal UserDetailsImpl userDetails) {
 
-        cardService.CardDeleteService(boardId, todoId, cardId);
+        CardResponseDto cardResponseDto = cardService.CardDeleteService(boardId, todoId, card_id, userDetails.getUser());
 
-        return ResponseEntity.ok(CommonResponse.<Void>builder()
+        return ResponseEntity.ok(CommonResponse.<CardResponseDto>builder()
             .msg("카드 삭제에 성공하였습니다.")
             .statusCode(HttpStatus.OK.value())
+            .data(cardResponseDto)
             .build());
     }
 
-    @PutMapping("/{cardId}")
+    @PutMapping("/boards/{board_id}/columns/{column_id}/cards/{card_id}")
     public ResponseEntity<CommonResponse<CardResponseDto>> updateCard(
-        @PathVariable Long boardId,
-        @PathVariable Long todoId,
-        @PathVariable Long cardId, @RequestBody CardRequestDto cardRequestDto) {
+        @PathVariable("board_id") Long boardId,
+        @PathVariable("column_id") Long todoId,
+        @PathVariable("card_id") Long card_id,
+        @RequestBody CardRequestDto cardRequestDto,
+        @AuthenticationPrincipal UserDetailsImpl userDetails) {
 
-        CardResponseDto cardResponseDto = cardService.CardUpdateService(boardId, todoId, cardId,
-            cardRequestDto);
+        CardResponseDto cardResponseDto = cardService.CardUpdateService(boardId, todoId, card_id,
+            cardRequestDto, userDetails.getUser());
 
         return ResponseEntity.ok(CommonResponse.<CardResponseDto>builder()
             .msg("카드 수정에 성공하였습니다.")
             .statusCode(HttpStatus.OK.value())
             .data(cardResponseDto)
             .build());
+    }
+
+    @PostMapping("/boards/{board_id}/columns/{column_id}/cards/{card_id}/inviteAuthor")
+    public ResponseEntity<CommonResponse<CardResponseDto>> inviteAuthor(
+        @PathVariable("board_id") Long boardId,
+        @PathVariable("column_id") Long todoId,
+        @PathVariable("card_id") Long card_id,
+        @RequestBody CardRequestDto cardRequestDto,
+        @AuthenticationPrincipal UserDetailsImpl userDetails) {
+
+        CardResponseDto cardResponseDto = cardService.inviteAuthor(boardId,todoId,card_id,cardRequestDto,userDetails.getUser());
+
+        return ResponseEntity.ok()
+            .body(CommonResponse.<CardResponseDto>builder()
+                .msg("카드 초대에 성공하였습니다.")
+                .statusCode(HttpStatus.OK.value())
+                .data(cardResponseDto)
+                .build());
     }
 }
--- src/main/java/com/nbc/trello/domain/card/CardService.java
+++ src/main/java/com/nbc/trello/domain/card/CardService.java
@@ -1,10 +1,22 @@
 package com.nbc.trello.domain.card;
 
+import com.nbc.trello.domain.author.Author;
+import com.nbc.trello.domain.author.AuthorRepository;
 import com.nbc.trello.domain.comment.Comment;
 import com.nbc.trello.domain.comment.CommentRepository;
+import com.nbc.trello.domain.comment.CommentService;
+import com.nbc.trello.domain.participants.Participants;
+import com.nbc.trello.domain.participants.ParticipantsRepository;
 import com.nbc.trello.domain.todo.Todo;
 import com.nbc.trello.domain.todo.TodoRepository;
+import com.nbc.trello.domain.user.User;
+import com.nbc.trello.domain.user.UserRepository;
+import com.nbc.trello.global.util.UserDetailsImpl;
+import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,24 +27,39 @@ public class CardService {
     private final CardRepository cardRepository;
     private final CommentRepository commentRepository;
     private final TodoRepository todoRepository;
+    private final UserRepository userRepository;
+    private final ParticipantsRepository participantsRepository;
+    private final AuthorRepository authorRepository;
 
     //카드 등록
-    public CardResponseDto CardCreateService(Long boardId, Long columnId,
-        CardRequestDto cardRequestDto) {
+    public CardResponseDto CardCreateService(Long boardId, Long todoId,
+        CardRequestDto cardRequestDto, User user) {
+        //참여자 검증
+        //validateParticipants(boardId, user);
+
         Card card = new Card(cardRequestDto);
-        Todo todo = todoRepository.findById(columnId).get();
+
+        //칼럽이 있는지 검증
+        Todo todo = todoRepository.findById(todoId).orElseThrow(()->new IllegalArgumentException("todo에 등록할 todo를 찾을 수 없습니다."));
         card.setTodo(todo);
         Card save = cardRepository.save(card);
+
+        //카드 생성할 때 레파지토리 작업자 리파지토리에 생성
+        authorRepository.save(new Author(user.getId(),card.getId(),user.getEmail()));
 
         return new CardResponseDto(boardId, todo.getId(), save.getId());
     }
 
     //카드 단건조회
-    public CardCommentResponseDto CardGetService(Long boardId, Long todoId, Long cardId) {
+    public CardCommentResponseDto CardGetService(Long boardId, Long todoId, Long cardId,UserDetailsImpl userDetails) {
+        //참여자 검증
+        validateParticipants(boardId, userDetails.getUser());
+        //투두 있는지
+        validateTodoExistInBoard(boardId, todoId);
 
-        Card card = cardRepository.findById(cardId).get();
+        //카드가 있으면 조회
+        Card card = cardRepository.findById(cardId).orElseThrow(()->new IllegalArgumentException("조회할 수 있는 카드가 없습니다."));
         List<Comment> byCardId = commentRepository.findByCardId(cardId);
-
         CardCommentResponseDto cardCommentResponseDto = new CardCommentResponseDto(card);
 
         for (Comment com : byCardId) {
@@ -49,28 +76,114 @@ public class CardService {
     }
 
     //카드 삭제
-    public void CardDeleteService(Long boardId, Long columnId, Long cardId) {
+    public CardResponseDto CardDeleteService(Long boardId, Long todoId, Long cardId, User user) {
 
-        Card card = cardRepository.findById(cardId).get();
+        //참여자 검증
+        validateParticipants(boardId,user);
 
-        if (columnId.equals(card.getTodo().getId())) {
+        //todo 검증
+        validateTodoExistInBoard(boardId,todoId);
+
+
+
+        Card card = cardRepository.findById(cardId).orElseThrow(()->new IllegalArgumentException("삭제할 카드가 존재하지 않습니다."));
+
+        //작업자 검증
+        validateAuthor(cardId,user.getId());
+
+        if (Objects.equals(todoId, card.getTodo().getId())) {
             cardRepository.delete(card);
+            List<Author> authorList = authorRepository.findByCardId(cardId).orElseThrow(()->new IllegalArgumentException("존재하지 않는 작업자입니다."));
+
+            //작업자 삭제
+            authorRepository.deleteAll(authorList);
         }
+
+        return new CardResponseDto(boardId, todoId, cardId);
     }
 
 
     //카드 수정
-    public CardResponseDto CardUpdateService(Long boardId, Long columnId, Long cardId,
-        CardRequestDto cardRequestDto) {
+    public CardResponseDto CardUpdateService(Long boardId, Long todoId, Long cardId,
+        CardRequestDto cardRequestDto, User user) {
 
-        Card card = cardRepository.findById(cardId).get();
+        //참여자 검증
+        //validateParticipants(boardId,user);
 
-        if (columnId.equals(card.getTodo().getId())) {
+        //todo 검증
+        //validateTodoExistInBoard(boardId,todoId);
+
+        Card card = cardRepository.findById(cardId).orElseThrow(()->new IllegalArgumentException("수정할 카드가 존재하지 않습니다."));
+
+        //작업자 검증
+        validateAuthor(cardId,user.getId());
+
+
+        if (Objects.equals(todoId, card.getTodo().getId())) {
             card.CardUpdate(cardRequestDto);
             Card save = cardRepository.save(card);
         }
 
         return new CardResponseDto(boardId, card.getTodo().getId(), card.getId());
+    }
+
+    public CardResponseDto inviteAuthor(Long boardId, Long todoId, Long cardId,
+        CardRequestDto cardRequestDto, User user){
+        //참여자 검증
+        // validateParticipants(boardId,user);
+
+        //todo 검증
+        //validateTodoExistInBoard(boardId,todoId);
+
+        //card 검증
+        validateCardExistInTodo(todoId,cardId);
+
+        //나 자신의 초대
+        if (!authorRepository.existsByCardIdAndUserId(cardId, user.getId())) {
+            throw new IllegalArgumentException("자기 자신은 초대할 수 없습니다.");
+        }
+
+        Author byEmail = authorRepository.findByEmail(cardRequestDto.getPic()).get();
+
+        if (byEmail.equals(cardRequestDto.getPic())){
+            throw new IllegalArgumentException("이미 참가하고 있습니다.");
+        }
+
+        Author author = new Author(boardId, cardId, cardRequestDto.getPic());
+        authorRepository.save(author);
+
+        return null;
+    }
+
+
+
+    private User findUserBy(String email) {
+        return userRepository.findByEmail(email).orElseThrow(
+            () -> new EntityNotFoundException("User 가 존재하지 않습니다."));
+    }
+
+    private void validateParticipants(Long boardId, User user) {
+        if (!participantsRepository.existsByBoardIdAndUserId(boardId, user.getId())) {
+            throw new EntityExistsException("참여자가 존재하지 않습니다.");
+        }
+    }
+
+    private void validateCardExistInTodo(Long todoId, Long cardId) {
+        if (!cardRepository.existsByIdAndTodoId(cardId, todoId)) {
+            throw new EntityExistsException("Todo 에 Card 가 존재하지 않습니다.");
+        }
+    }
+
+    private void validateTodoExistInBoard(Long boardId, Long todoId) {
+        if (!todoRepository.existsByIdAndBoardId(todoId, boardId)) {
+            throw new EntityExistsException("Board 에 Todo 가 존재하지 않습니다.");
+        }
+    }
+
+    private void validateAuthor(Long cardId, Long userId) {
+        if (!authorRepository.existsByCardIdAndUserId(cardId, userId)) {
+            throw new IllegalArgumentException("작업자가 아닙니다.");
+        }
     }
 
 }
